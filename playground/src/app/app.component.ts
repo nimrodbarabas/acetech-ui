65c7392658ae661e63bb2adfaa6c347f7af76905
barabasnimrod <nimrod.barabas@acetech.dev>
2024-05-29 20:09:19 +0300

    first commit

--- playground/src/app/app.component.ts
+++ playground/src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit, Renderer2} from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import {ActButtonDirective, ActCardComponent, ActInputDirective, MyLibComponent} from "acetech-ui";
+import {DOCUMENT} from "@angular/common";
 
 @Component({
   standalone: true,
@@ -10,6 +11,13 @@ import {ActButtonDirective, ActCardComponent, ActInputDirective, MyLibComponent}
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'playground';
+
+  private readonly renderer = inject(Renderer2)
+  private readonly document = inject(DOCUMENT)
+
+  ngOnInit() {
+    this.renderer.addClass(this.document.documentElement, "light");
+  }
 }
